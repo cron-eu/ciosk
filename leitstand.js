58e24009536637f471105aca460075f69899873a
Sebastian Krüger <sk@cron.eu>
2017-02-10 13:30:00 +0100

    Changed API usage

--- leitstand.js
+++ leitstand.js
@@ -56,38 +56,24 @@ module.exports = function (leitstand) {
       }
     })
     .plugin('demo', {
-      widget: function () {
-        return {
-          put: false,
-          spec: {
-            second: [
-              Math.floor((Math.random() * 60)),
-              Math.floor((Math.random() * 60)),
-              Math.floor((Math.random() * 60)),
-              Math.floor((Math.random() * 60)),
-              Math.floor((Math.random() * 60)),
-              Math.floor((Math.random() * 60)),
-              Math.floor((Math.random() * 60))
-            ].filter(function(el, i, a){
-              return i == a.indexOf(el);
-            }).sort(function(a, b) {
-              return a - b;
-            })
-          },
-          job: function () {
-            this.set({
-              value: Math.floor((Math.random() * 100) + 1)
-            });
+      spec: {
+        second: [
+          Math.floor((Math.random() * 60)),
+          Math.floor((Math.random() * 60))
+        ].filter(function(el, i, a){
+          return i == a.indexOf(el);
+        }).sort(function(a, b) {
+          return a - b;
+        })
+      },
+      api: function (settings, callback) {
+        callback({
+          random: function() {
+            return Math.floor((Math.random() * 100) + 1);
           }
-        };
-      }
-    }, function () {
-      for (i = 0; i < 11; i++) {
-        leitstand.widget(i + 1, this.widget());
+        });
       }
     })
-    .dashboard('default', function () {
-      // /dashboards/default
-      this.add('.*');
-    });
+    .widget('demo-widget', {plugin: 'demo', method: 'random'})
+    .dashboard('default', {widgets: '.*'});
 };
